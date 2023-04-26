56c154210e21d73388b22880379e1920cd835f08
JULIAVALERIO <juliav286@gmail.com>
2023-04-25 21:18:46 -0300

    Atualizando politicas de CORS

--- src/main/java/com/projeto/cardapio/CardapioApplication.java
+++ src/main/java/com/projeto/cardapio/CardapioApplication.java
@@ -2,7 +2,11 @@ package com.projeto.cardapio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+import com.projeto.cardapio.config.CorsConfig;
+
+@Import(CorsConfig.class)
 @SpringBootApplication
 public class CardapioApplication {
 
